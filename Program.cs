12af0c55f7cd9146a6498ab31e0977733268374b
Kirzahk <61593550+Kirzahk@users.noreply.github.com>
2020-03-01 20:39:54 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace Ejemplo08___argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 4)
+            {
+                WriteLine("Tienes que indicar 2 colores y dimensiones");
+                return; //detiene la ejecución
+            }
+            ForegroundColor = (ConsoleColor)Enum.Parse(
+                enumType: typeof(ConsoleColor), 
+                value: args[0], 
+                ignoreCase: true);
+
+            BackgroundColor = (ConsoleColor)Enum.Parse(
+                enumType: typeof(ConsoleColor),
+                value: args[1],
+                ignoreCase: true);
+            WindowWidth = int.Parse(args[2]);
+            WindowHeight = int.Parse(args[3]);
+        }
+    }
+}
